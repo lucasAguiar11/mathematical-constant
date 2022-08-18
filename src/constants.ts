2f35ad835572c5014943213cae371bc6b0c97b3d
Lucas Aguiar <lucas.abreu@redeceler.com.br>
2022-08-18 16:05:43 -0300

    Readme

--- src/constants.ts
+++ src/constants.ts
@@ -1,36 +1,42 @@
 import fs from 'fs'
+import path from 'path'
+
+const readFile = (pFile: string): string => {
+  const file = fs.readFileSync(path.resolve(__dirname, pFile));
+  return file.toString()
+}
 
 export const eulerDigits = (): string => {
-  const numbers = fs.readFileSync('./data/e.txt', 'utf8')
+  const numbers = readFile('../data/e.txt')
   return numbers
 }
 
 export const eulerMascheroniDigits = (): string => {
-  const numbers = fs.readFileSync('./data/γ.txt', 'utf8')
+  const numbers = readFile('../data/γ.txt')
   return numbers
 }
 
 export const piDigits = (): string => {
-  const numbers = fs.readFileSync('./data/π.txt', 'utf8')
+  const numbers = readFile('../data/π.txt')
   return numbers
 }
 
 export const pythagorasDigits = (): string => {
-  const numbers = fs.readFileSync('./data/√2.txt', 'utf8')
+  const numbers = readFile('../data/√2.txt')
   return numbers
 }
 
 export const theodorusDigits = (): string => {
-  const numbers = fs.readFileSync('./data/√3.txt', 'utf8')
+  const numbers = readFile('../data/√3.txt')
   return numbers
 }
 
 export const squareRoot5Digits = (): string => {
-  const numbers = fs.readFileSync('./data/√5.txt', 'utf8')
+  const numbers = readFile('../data/√5.txt')
   return numbers
 }
 
 export const goldenRatioDigits = (): string => {
-  const numbers = fs.readFileSync('./data/Φ.txt', 'utf8')
+  const numbers = readFile('../data/Φ.txt')
   return numbers
 }
